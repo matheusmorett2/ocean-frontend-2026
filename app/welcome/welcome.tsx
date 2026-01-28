ffbe3b51786243a9af9221d688ffee1a149d27a3
Matheus Morett <matheusmorett300@gmail.com>
2026-01-28 00:55:27 +0000

    Iniciando meu site do Rick and Morty =)

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,11 +1,27 @@
 export function Welcome() {
-  const titulo = "Hello Ocean!!";
-
   return (
-    <div>
-      <h1>
-        {titulo}
-      </h1>
-    </div>
+    <>
+      <div className="header">
+        <h1>
+          Rick and Morty
+        </h1>
+      </div>
+      <div className="personagens">
+        <div className="personagem">
+          <img 
+            src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" 
+          />
+          <p>
+            Rick Sanchez
+          </p>
+          <p>
+            Human
+          </p>
+          <span>
+            Alive
+          </span>
+        </div>
+      </div>
+    </>
   );
 }
